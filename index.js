529d54e09bf85d4621bf21b2c6e751eb65df3931
MONYtry <binderleon88@gmail.com>
2026-09-24 07:16:37 +0200

    Added Comments

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function test() 
 {
+    // Holt benötigte Elemente
     const data = document.getElementById("inputfield");
     const display = document.getElementById("display");
 
     try 
     {
+        // Versucht alle Buchstaben zu teilen
         var test = data.value.split('');
-        console.log(test);
+        
+        // Setzt den Display zurück
         display.value = "";
+        
+        //  Holt sich für jeden Buchstaben die angegebe Dezimalzahl
         for (let i = 0; i < test.length; i++) 
         {
             getDec(test[i]);
@@ -19,27 +24,36 @@ function test()
         console.log("Fehler: " + error);
     }
 }
-
+// Hilfs Arrays für vereinfachteren Ablauf
 const bit = [128, 64, 32, 16, 8, 4, 2, 1];
 const currentDisplay = [];
 
 function getDec(charArray) 
 {
+    // Holt sich die angegebene Zahl (Library von JS selbst)
     let zahl = charArray.charCodeAt(0);
+
+    // Versucht aus der angegebenen Zahl eine Binäre zu holen
     getBinary(zahl);
 }
 
 
 function getBinary(currentChar) 
 {
+    // Bessere Übersicht
     clearDisplay();
-
-    for (let i = 0; i < bit.length; i++) {
+    // Geht durch jeden verfügbaren Bit 
+    for (let i = 0; i < bit.length; i++)
+    {
+        // Wenn currentChar größer oder gleich ist 
         if (currentChar >= bit[i]) {
             console.log("1");
+            // Subtrahieren und danach damit weiter rechen
             currentChar -= bit[i];
+            // Fügt es dem Display-Array hinzu
             currentDisplay.push("1");
         }
+        // Wenn es nicht geht 0 speichern
         else {
             console.log("0");
             currentDisplay.push("0");
@@ -49,6 +63,8 @@ function getBinary(currentChar)
 }
 
 function addDisplay() {
+    // Fügt den Wert hinzu
+    // Es wird alles unnötige durch join("") entfernt
     display.value += currentDisplay.join("") + " ";
     clearDisplay();
 }
